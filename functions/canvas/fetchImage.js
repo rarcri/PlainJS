2359245307e881e75432e724311aa577f1d40f7e
rarcri <rarcri@protonmail.com>
2021-09-06 00:09:39 +0300

    Update

--- functions/canvas/fetchImage.js
+++ functions/canvas/fetchImage.js
@@ -1,6 +1,6 @@
 function fetchImage(searchTerms) {
   return fetch(
-    `https://pixabay.com/api/?key=${process.env.APIKEY}&q=${
+    `https://pixabay.com/api/?key=15219428-7eba042dad533469cf2a7368c&q=${
       searchTerms[Math.floor(Math.random() * searchTerms.length)]
     }&image_type=photo&per_page=200`
   ).then((res) => res.json());
